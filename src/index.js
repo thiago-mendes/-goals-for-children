a87cc190a46ef4a6d9baa887ee0a54943744dee1
Thiago Mendes <almeida16tm@gmail.com>
2018-12-28 00:46:00 -0200

    Adding Sass to the project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "antd/dist/antd.css";
+import "./assets/css/index.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
